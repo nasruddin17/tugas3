a9927d9d49b1ba09850c979f48a40e1f9978240d
nasruddin17 <43714390+nasruddin17@users.noreply.github.com>
2018-10-31 23:57:03 +0800

    Create MainQueueString.java

---
+++ MainQueueString.java
@@ -0,0 +1,15 @@
+package ASD3;
+import sun.misc.Queue;
+public class MainQueueString {
+
+	public static void main(String[] args) {
+		QueueString q = new QueueString();
+		q.cetak();
+		
+		q.insert("NAS");q.cetak();
+		q.insert("YUSUF");q.cetak();
+		q.insert("ABDI");q.cetak();
+		q.insert("KADRI");q.cetak();
+
+	}
+}
